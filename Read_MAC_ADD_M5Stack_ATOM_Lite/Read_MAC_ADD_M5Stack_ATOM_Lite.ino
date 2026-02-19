0e84021a4c3f5bb53554c27c2d0104ec0586749d
lxmill <mauricio@mill.pt>
2026-02-19 10:15:27 +0000

    Create Read_MAC_ADD_M5Stack_ATOM_Lite.ino

---
+++ Read_MAC_ADD_M5Stack_ATOM_Lite/Read_MAC_ADD_M5Stack_ATOM_Lite.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include "esp_mac.h"      // ESTE é o header correto
+#include "esp_system.h"
+
+void setup() {
+
+    Serial.begin(115200);
+    delay(1000);
+
+    uint8_t mac[6];
+
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+
+    Serial.printf("MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                  mac[0],
+                  mac[1],
+                  mac[2],
+                  mac[3],
+                  mac[4],
+                  mac[5]);
+}
+
+void loop() {
+}
